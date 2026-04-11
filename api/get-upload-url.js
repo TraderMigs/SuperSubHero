0abe32406e4d60c875c0701ab99efbd1008eb271
TraderMigs <wvdreamsofimd@gmail.com>
2026-04-11 17:20:44 +0700

    Update get-upload-url.js

--- api/get-upload-url.js
+++ api/get-upload-url.js
@@ -5,6 +5,7 @@ const R2_ACCOUNT_ID = process.env.R2_ACCOUNT_ID;
 const R2_ACCESS_KEY_ID = process.env.R2_ACCESS_KEY_ID;
 const R2_SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY;
 const R2_BUCKET = "supersubhero-videos";
+const R2_PUBLIC_URL = "https://pub-23dc0cc0dda3466d85d541f4b669c30d.r2.dev";
 
 const s3 = new S3Client({
   region: "auto",
@@ -32,8 +33,7 @@ export default async function handler(req, res) {
     });
 
     const signedUrl = await getSignedUrl(s3, command, { expiresIn: 3600 });
-
-    const publicUrl = `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com/${R2_BUCKET}/${key}`;
+    const publicUrl = `${R2_PUBLIC_URL}/${key}`;
 
     return res.status(200).json({ signedUrl, publicUrl, key });
   } catch (err) {
